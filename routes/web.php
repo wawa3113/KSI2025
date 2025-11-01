34f7fcdc4e522de0e2d882541fdd3d9a42dc7ec2
wawa3113 <sherlynanazwamutiya@gmail.com>
2025-11-01 11:09:31 +0700

    Update file Laravel (web.php dan lainnya)

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,29 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return 'Hello World';
+    return '
+        <html>
+            <head>
+                <title>Hello World</title>
+                <style>
+                    body {
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                        height: 100vh;
+                        background-color: #fef6fb;
+                        font-family: Arial, sans-serif;
+                    }
+                    h1 {
+                        font-size: 3rem;
+                        font-weight: bold;
+                        color: #e91e63; /* warna pink biar cantik */
+                    }
+                </style>
+            </head>
+            <body>
+                <h1>Hello World</h1>
+            </body>
+        </html>
+    ';
 });
