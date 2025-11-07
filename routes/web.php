42393cadd79f424589c8ee016f1f86c65cf95264
wawa3113 <sherlynanazwamutiya@gmail.com>
2025-11-07 17:19:52 +0700

    Menambahkan fitur koneksi database pada branch KoneksiDatabase

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HelloController;
 
-Route::get('/', function () {
-    return '
-        <html>
-            <head>
-                <title>Hello World</title>
-                <style>
-                    body {
-                        display: flex;
-                        justify-content: center;
-                        align-items: center;
-                        height: 100vh;
-                        background-color: #fef6fb;
-                        font-family: Arial, sans-serif;
-                    }
-                    h1 {
-                        font-size: 3rem;
-                        font-weight: bold;
-                        color: #e91e63; /* warna pink biar cantik */
-                    }
-                </style>
-            </head>
-            <body>
-                <h1>Hello World</h1>
-            </body>
-        </html>
-    ';
-});
+Route::get('/', [HelloController::class, 'index']);
